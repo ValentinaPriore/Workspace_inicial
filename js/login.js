345880e069814883b90e739565732ef3df66a318
ValentinaPriore <valentinaprioref@gmail.com>
2020-08-30 19:23:48 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,3 +4,17 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+//Fiesta
+const themeSwitch = document.getElementById("theme-switch");
+const body = document.getElementById("body");
+
+themeSwitch.addEventListener("click", () => {
+	let currentBodyClass = body.className;
+
+	if (body.className === "light") {
+		body.className = "dark";
+	} else if (body.className === "dark") {
+		body.className = "light";
+	}
+});
